51cdfb365172059c02429ad39acf00960d82668e
Furkan Kılıc <fkrx928@gmail.com>
2023-12-21 13:42:54 +0300

    my commit

--- src/test/java/base_urls/HerOkuAppBaseUrl.java
+++ src/test/java/base_urls/HerOkuAppBaseUrl.java
@@ -7,12 +7,15 @@ import org.junit.Before;
 public class HerOkuAppBaseUrl {
 
 
+
     protected RequestSpecification spec;
     @Before//Her test den önce çalışacak
     public void setUp(){
 
-        String baseUrl = "https://jsonplaceholder.typicode.com";
+        String baseUrl = "https://restful-booker.herokuapp.com";
         spec= new RequestSpecBuilder().setBaseUri(baseUrl).build();
+
+
 
 
     }
--- src/test/java/get_requests/Get06.java
+++ src/test/java/get_requests/Get06.java
@@ -1,11 +1,56 @@
 package get_requests;
 
-public class Get06 {
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Get06 extends HerOkuAppBaseUrl {
 
 
+    /*
+       Given
+           https://restful-booker.herokuapp.com/booking
+       When
+           User sends get request to the URL
+       Then
+           Status code is 200
+       And
+           Among the data there should be someone whose firstname is "John" and lastname is "Smith"
+    */
+
+    @Test
+    public void name() {
+
+        // i) Url kurulacak
+        spec.pathParam("first","booking")
+                .queryParams("firstname","John"
+                        ,"lastname","Smith");
+
+        // ii) Beklenen data belirlenecek
+        // iii) Request gönderilip Response alınacak
+        Response response =  given(spec).when().get("{first}");
+        response.prettyPrint();
+
+        // iv) Doğrulamalar yapılacak
+        // 1. yol:
+        response
+                .then()
+                .statusCode(200)
+                .body("bookingid",hasSize(greaterThan(0)))
+                .body(containsString("bookingid"));
 
 
+        // 2. yol:
+        assertEquals(200,response.statusCode());
+        assertTrue(response.asString().contains("bookingid"));
 
 
-
+    }
 }
