03eed7af6b6a4727488600a1823c29f32885f750
Furkan Kılıc <fkrx928@gmail.com>
2023-12-21 13:44:17 +0300

    my commit

--- src/test/java/get_requests/Get07.java
+++ src/test/java/get_requests/Get07.java
@@ -1,4 +1,38 @@
 package get_requests;
 
 public class Get07 {
+
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/11
+        When
+            User send a GET request to the URL
+        Then
+            HTTP Status Code should be 200
+        And
+            Response content type is "application/json"
+        And
+            Response body should be like;
+                        {
+                            "firstname": "John",
+                            "lastname": "Smith",
+                            "totalprice": 111,
+                            "depositpaid": true,
+                            "bookingdates": {
+                                "checkin": "2018-01-01",
+                                "checkout": "2019-01-01"
+                            },
+                            "additionalneeds": "Breakfast"
+                        }
+     */
+
+
+
+
+
+
+
+
+
+
 }
