5de49c2ac64b2adc39383c3f252927e3eea07d25
Furkan Kılıc <fkrx928@gmail.com>
2023-12-22 22:19:48 +0300

    my commit

--- src/test/java/get_requests/Get08.java
+++ src/test/java/get_requests/Get08.java
@@ -1,4 +1,49 @@
 package get_requests;
 
 public class Get08 {
+
+/*
+    Given
+        https://petstore.swagger.io/v2/pet/235
+    When
+         User send a GET request to the URL
+    Then
+        HTTP Status Code should be 200
+    And
+        Response content type is "application/json"
+    And
+        Response body should be like;
+           {
+            "id": 235,
+            "category": {
+                "id": 0,
+                "name": "Aldo"
+            },
+            "name": "Dog",
+            "photoUrls": [
+                "www.ornekolacakyadım1.com",
+                "www.ornekolacakyadım2.com"
+            ],
+            "tags": [
+                {
+                    "id": 0,
+                    "name": "Seven"
+                },
+                {
+                    "id": 0,
+                    "name": "Eight"
+                }
+            ],
+            "status": "pending"
+           }
+
+     */
+
+
+
+
+
+
+
+
 }
