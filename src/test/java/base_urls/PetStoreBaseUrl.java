3d41a068543ea861e757fa699a96113f288023b9
Furkan Kılıc <fkrx928@gmail.com>
2023-12-22 22:13:36 +0300

    my commit

--- src/test/java/base_urls/PetStoreBaseUrl.java
+++ src/test/java/base_urls/PetStoreBaseUrl.java
@@ -1,4 +1,15 @@
 package base_urls;
 
+import io.restassured.builder.RequestSpecBuilder;
+import org.junit.Before;
+
 public class PetStoreBaseUrl {
+
+
+
+    @Before
+    public void setUp(){
+        String baseUrl ="https://petstore.swagger.io/v2";
+        spec = new RequestSpecBuilder().setBaseUri(baseUrl).build();
+    }
 }
