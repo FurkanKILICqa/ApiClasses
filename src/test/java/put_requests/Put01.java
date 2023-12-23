f3b574b5d746317147e9ab2f3e2a19e25fc0351f
Furkan Kılıc <fkrx928@gmail.com>
2023-12-23 22:47:38 +0300

    my commit

--- src/test/java/put_requests/Put01.java
+++ src/test/java/put_requests/Put01.java
@@ -1,10 +1,14 @@
 package put_requests;
 
 import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 import static test_data.JsonPlaceHolderTestData.jsonPlaceHolderMapper;
 
 public class Put01 extends JsonPlaceHolderBaseUrl {
@@ -35,6 +39,20 @@ public class Put01 extends JsonPlaceHolderBaseUrl {
 
 
         Map<String,Object> payLoad = jsonPlaceHolderMapper(55,"Wash the dishes",false);
+
+        // Request Göderip Response alınacak:
+        Response response = given(spec).body(payLoad).when().put("{first}/{second}");
+        response.prettyPrint();
+        // Serialization: Java objesini Json Objesine dönüştürme işlemine denir
+        // Serialization, Serializer denen JAckson Databind, Gson , Yasson gibi dependencyleri pom a yüklemek ile halledilir
+
+        // Doğrulamalar Yapılır
+        Map<String,Object> actualData =  response.as(HashMap.class);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(payLoad.get("userId"),actualData.get("userId"));
+        assertEquals(payLoad.get("title"),actualData.get("title"));
+        assertEquals(payLoad.get("completed"),actualData.get("completed"));
 
 
     }
