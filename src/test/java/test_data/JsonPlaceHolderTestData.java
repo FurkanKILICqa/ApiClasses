a65ec8154553b610917935ba94dfded83aec5036
Furkan Kılıc <fkrx928@gmail.com>
2023-12-23 22:03:30 +0300

    my commit

--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -1,7 +1,19 @@
 package test_data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JsonPlaceHolderTestData {
 
 
+    public static Map<String,Object> jsonPlaceHolderMapper(int userId,String title,boolean complated){
+
+        Map<String,Object> map = new HashMap<>();
+        map.put("userId",userId);
+        map.put("title",title);
+        map.put("completed",complated);
+
+        return map;
+    }
 
 }
