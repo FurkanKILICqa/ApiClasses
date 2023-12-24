8a5f373ec79bdff83056ec6791f35e14a34031ac
Furkan Kılıc <fkrx928@gmail.com>
2023-12-24 23:58:49 +0300

    my commit

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -35,7 +35,7 @@ public class Post02 extends JsonPlaceHolderBaseUrl {
         Serialization : Java objesini Json Objesine dönüştüme işlmeine denirsdsf
         */
 
-        // Serialization: Java objesini Json Objesine dönüştürme işlemine denirasfasf
+        // Serialization: Java objesini Json Objesine dönüştürme işlemine denir
         // Serialization, Serializer denen JAckson Databind, Gson , Yasson gibi dependencyleri pom a yüklemek ile halledilir
 
         //Doğrular yapılır
