0f9c88673b75a894b55cee6e090fd872a8acb0d8
Furkan Kılıc <fkrx928@gmail.com>
2023-12-24 23:59:19 +0300

    my commit

--- src/test/java/post_requests/Post02.java
+++ src/test/java/post_requests/Post02.java
@@ -48,7 +48,7 @@ public class Post02 extends JsonPlaceHolderBaseUrl {
         assertEquals(payload.get("completed"),actualData.get("completed"));
     }
 
-    // Post01 deki task map oluşturmayı metod içinde hallederek geliştirildisxs
+    // Post01 deki task map oluşturmayı metod içinde hallederek geliştirildi
     @Test
     public void postMap(){
         // Url Oluşturulacak
