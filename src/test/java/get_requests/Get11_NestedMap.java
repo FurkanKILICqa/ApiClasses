a4915313405f4c3e531dfe2ea770c2f1689ef310
Furkan Kılıc <fkrx928@gmail.com>
2023-12-26 20:56:21 +0300

    my commit

--- src/test/java/get_requests/Get11_NestedMap.java
+++ src/test/java/get_requests/Get11_NestedMap.java
@@ -1,4 +1,25 @@
 package get_requests;
 
 public class Get11_NestedMap {
+
+
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/51
+        When
+            I send GET Request to the url
+        Then
+            Response body should be like that;
+                {
+                    "firstname": "Jane",
+                    "lastname": "Doe",
+                    "totalprice": 111,
+                    "depositpaid": true,
+                    "bookingdates": {
+                        "checkin": "2018-01-01",
+                        "checkout": "2019-01-01"
+                    },
+                    "additionalneeds": "Extra pillows please"
+                }
+     */
 }
