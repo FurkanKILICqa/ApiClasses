10dda1125e4457a9dd18e9f14c2cc4341380c165
Furkan Kılıc <fkrx928@gmail.com>
2023-12-26 22:07:05 +0300

    my commit

--- src/test/java/test_data/HerokuAppTestData.java
+++ src/test/java/test_data/HerokuAppTestData.java
@@ -1,7 +1,23 @@
 package test_data;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class HerokuAppTestData {
 
+
+
+    public static Map<String,String> bookingMapper(String checkin){
+        Map<String,String> bookingMap = new HashMap<>();
+        bookingMap.put("checkin","2018-01-01");
+        bookingMap.put("checkout","2019-01-01");
+        System.out.println("bookingMap = " + bookingMap);
+
+
+        return bookingMap;
+    }
 
 
 
