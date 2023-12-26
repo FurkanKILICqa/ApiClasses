f9c029bc87f498b0cc137ec2a5f3bc5e5886279a
Furkan Kılıc <fkrx928@gmail.com>
2023-12-26 22:05:05 +0300

    my commit

--- src/test/java/get_requests/Get11_NestedMap.java
+++ src/test/java/get_requests/Get11_NestedMap.java
@@ -88,11 +88,60 @@ public class Get11_NestedMap extends JsonPlaceHolderBaseUrl {
         assertEquals(expectedData.get("additionalneeds"),actualData.get("additionalneeds"));
     }
 
+    @Test
+    public void name2() {
+        // Url Kurulur:
+        spec.pathParams("first","booking"
+                ,"second",50) ;
+
+        // Beklenen data kurulur:
+
+        // Nested yapılarda beklenen data en içteki yapıdan başlayarak oluşturulur
+
+        Map<String,String> bookingMap = new HashMap<>();
+        bookingMap.put("checkin","2018-01-01");
+        bookingMap.put("checkout","2019-01-01");
+        System.out.println("bookingMap = " + bookingMap);
+
+        Map<String,Object> expectedData = new HashMap<>();
+        expectedData.put("firstname","Jane");
+        expectedData.put("lastname","Doe");
+        expectedData.put("totalprice",111);
+        expectedData.put("depositpaid",true);
+        expectedData.put("bookingdates",bookingMap);
+        expectedData.put("additionalneeds","Extra pillows please");
+        System.out.println("expectedData = " + expectedData);
+
+        // Request---- Response
+        Response response = given(spec).when().get("{first}/{second}");
+        response.prettyPrint();
 
 
 
 
+        // Doğrulamalar yapılır:
+        response
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo(expectedData.get("firstname")))
+                .body("bookingdates.checkin",equalTo(bookingMap.get("checkin")));
+
+        JsonPath json = response.jsonPath();
+        assertEquals(expectedData.get("firstname"),json.getString("firstname"));
+        assertEquals(bookingMap.get("checkin"),json.getString("bookingdates.checkin"));
+
+        Map<String,Object> actualData = response.as(HashMap.class);
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedData.get("firstname"),actualData.get("firstname"));
+        assertEquals(expectedData.get("lastname"),actualData.get("lastname"));
+        assertEquals(expectedData.get("totalprice"),actualData.get("totalprice"));
+        assertEquals(expectedData.get("depositpaid"),actualData.get("depositpaid"));
+        assertEquals(((Map)expectedData.get("bookingdates")).get("checkin") ,((Map)actualData.get("bookingdates")) .get("checkin"));
+        assertEquals(bookingMap.get("checkout"),((Map)actualData.get("bookingdates")).get("checkout"));
+        assertEquals(expectedData.get("additionalneeds"),actualData.get("additionalneeds"));
 
 
     }
+}
+
 
