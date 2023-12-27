211d8ac688a31e9e0bf276fc258066b3124290bf
Furkan Kılıc <fkrx928@gmail.com>
2023-12-27 12:17:58 +0300

    my commit

--- src/test/java/pojos/JsonPlaceHolderPojo.java
+++ src/test/java/pojos/JsonPlaceHolderPojo.java
@@ -1,4 +1,34 @@
 package pojos;
 
+import org.junit.Test;
+
 public class JsonPlaceHolderPojo {
+
+    /*
+         Given
+            https://jsonplaceholder.typicode.com/todos
+            {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+            }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+
+
+     */
+
+    @Test
+    public void name() {
+
+    }
 }
