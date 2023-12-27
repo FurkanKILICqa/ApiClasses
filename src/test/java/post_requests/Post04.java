d722bc0f3d72d9c5ead0826599fb35cca090a465
Furkan Kılıc <fkrx928@gmail.com>
2023-12-27 12:32:35 +0300

    my commit

--- src/test/java/post_requests/Post04.java
+++ src/test/java/post_requests/Post04.java
@@ -1,10 +1,39 @@
 package post_requests;
 
-public class Post04 {
+import base_urls.JsonPlaceHolderBaseUrl;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+
+public class Post04 extends JsonPlaceHolderBaseUrl {
+
+
+    /*
+         Given
+            https://jsonplaceholder.typicode.com/todos
+            {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+            }
+        When
+            I send POST Request to the Url
+        Then
+            Status code is 201
+        And
+            response body is like {
+                                    "userId": 55,
+                                    "title": "Tidy your room",
+                                    "completed": false,
+                                    "id": 201
+                                    }
+     */
+
+    @Test
+    public void name() {
 
 
 
 
 
-
+    }
 }
