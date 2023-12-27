17278b5cb033e440435047d0ee38c0b1818d35ab
Furkan Kılıc <fkrx928@gmail.com>
2023-12-27 11:39:06 +0300

    my commit

--- src/test/java/patch_request/Patch01.java
+++ src/test/java/patch_request/Patch01.java
@@ -1,8 +1,17 @@
 package patch_request;
 
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
 
-public class Patch01 {
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Patch01 extends JsonPlaceHolderBaseUrl {
 
     /*
         Given
@@ -21,10 +30,28 @@ public class Patch01 {
 									    "id": 198
 									    }
      */
-
     @Test
-    public void name() {
+    public void patch(){
+        // Url Kurulacak:
+        spec.pathParams("first","todos","second",198);
 
+        // Beklenen data oluşturulur:
+        Map<String,Object> payLoad =  JsonPlaceHolderTestData.jsonPlaceHolderMapper(null,"Wash the dishes",null);
+        Map<String,Object> expectedData = JsonPlaceHolderTestData.jsonPlaceHolderMapper(10,"Wash the dishes",true);
+
+        System.out.println("payLoad = " + payLoad);
+
+        // Request ------ Response:
+        Response response = given(spec).body(payLoad).when().patch("{first}/{second}");
+        response.prettyPrint();
+
+        // Doğrulamalar yapılır:
+        Map<String,Object> actualData =  response.as(HashMap.class);
+
+        assertEquals(200,response.statusCode());
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
 
 
     }
