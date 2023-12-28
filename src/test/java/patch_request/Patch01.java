cfd78f4c8330234a1c49dbc2acdab6d87ef29c8c
Furkan Kılıc <fkrx928@gmail.com>
2023-12-28 23:50:09 +0300

    my commit

--- src/test/java/patch_request/Patch01.java
+++ src/test/java/patch_request/Patch01.java
@@ -17,7 +17,7 @@ public class Patch01 extends JsonPlaceHolderBaseUrl {
         Given
 	        1) https://jsonplaceholder.typicode.com/todos/198
 	        2) {
-                 "title": "Wash the dishes gg"
+                 "title": "Wash the dishes gg hh"
                }
         When
 	 		I send PATCH Request to the Url
