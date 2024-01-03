0f15a899833dfe27abbdda0123a418efa11477b5
Furkan Kılıc <fkrx928@gmail.com>
2024-01-03 23:56:46 +0300

    my commit

--- src/test/java/get_requests/Get06.java
+++ src/test/java/get_requests/Get06.java
@@ -18,7 +18,7 @@ public class Get06 extends HerOkuAppBaseUrl {
        Given
            https://restful-booker.herokuapp.com/booking
        When
-           User sends get request to the URL 11 22 33 44 55
+           User sends get request to the URL
        Then
            Status code is 200
        And
