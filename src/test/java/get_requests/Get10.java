5ed160576489b8ad3f688957c12c1f25b8d2df08
Furkan Kılıc <fkrx928@gmail.com>
2024-01-06 23:50:21 +0300

    my commit

--- src/test/java/get_requests/Get10.java
+++ src/test/java/get_requests/Get10.java
@@ -19,7 +19,7 @@ public class Get10 extends JsonPlaceHolderBaseUrl {
     When
         I send GET Request to the URL
     Then
-        Status code is 200 11 22 33 44 55 66
+        Status code is 200
     And
         "completed" is false
     And
