9937971df0eb5d5820d62e5b4e05dfdc317b0e98
Furkan Kılıc <fkrx928@gmail.com>
2024-01-09 23:46:46 +0300

    furkan

--- src/test/java/get_requests/Get04.java
+++ src/test/java/get_requests/Get04.java
@@ -15,7 +15,7 @@ public class Get04 extends JsonPlaceHolderBaseUrl {
         Given
             https://jsonplaceholder.typicode.com/todos/23
         When
-            User send GET Request to the URL adf 666
+            User send GET Request to the URL adf 666 996
         Then
             HTTP Status Code should be 200
 		And
